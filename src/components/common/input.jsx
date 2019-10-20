556ce2f233bacd5494c6ad49e0fd93b7e1080b11
shaukatiqbaljalal <you@example.com>
2019-10-20 20:06:39 +0500

    inputs errors size changed

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const Input = ({ name, label, error, classname = "form-control", ...rest }) => {
   return (
-    <div className="form-group ">
+    <div className="form-group">
       <label htmlFor={name}>{label}</label>
       <input {...rest} name={name} id={name} className={classname} />
-      {error && <div className="alert alert-danger">{error}</div>}
+      {error && (
+        <div className="alert alert-danger" style={{ fontSize: "12px" }}>
+          {error}
+        </div>
+      )}
     </div>
   );
 };
